07862352636ea7af28e38f499cc2a7a0131e803d
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-05-03 11:06:05 +0300

    class-intro

---
+++ DE09/javascript/classnotes/js01-intro-variables/app.js
@@ -0,0 +1,15 @@
+// console.log("external js den selamlar");
+
+/*birden 
+fazla satır
+yorumu*/
+
+console.log("burası console alanıdır");
+
+console.error("bu bir hata mesajıdır")
+
+console.warn("bu bir uyarı mesajıdır")
+
+// console.clear()  //consolda yazılanları temizler
+
+console.table([{name:"ipek", age:30},    {name:"ayse",age:45}])
