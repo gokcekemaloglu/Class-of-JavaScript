bb363163b13b8f252130ea7d19c999d04ca38b58
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-06-05 10:51:07 +0300

    "Updated app.js with different event listener callback styles and added styles to style.css"

--- DE09/javascript/classnotes/js17-Events/app.js
+++ DE09/javascript/classnotes/js17-Events/app.js
@@ -6,9 +6,42 @@ let sonuc = document.getElementById("result")
 
 //! Callback yazım çeşitleri
 
-buton1.addEventListener("click",function konsol(){
-    console.log("buton1 clicked");
-})
+//? 1. Yol:
+
+// buton1.addEventListener("click",function konsol(){
+//     console.log("buton1 clicked");
+// })
+
+//? 2. Yol: (Fonksiyon ismi opsiyonel)
+
+// buton1.addEventListener("click",function konsol(){
+//     console.log("buton1 clicked");
+//     sonuc.innerText += "Buton 1 clicked" ;
+     // sonuc.textContent += "Buton 1 clicked" ; // yukarıdaki ile ikisi aynı şeyi yapıyor
+// })
+
+// buton1.addEventListener("dblclick",function konsol(){
+    
+//     sonuc.textContent = "Buton 1 double clicked" ;
+//     console.log("buton1 double clicked");
+    
+// })
+
+//? 3. Yol: dışardan arrow / named(decleration) fonksiyon çağırma
+
+// const clicked2 = () => {sonuc.textContent = "Button 1 clicked"}
+
+// buton1.addEventListener("click",clicked2) //çağırma parantezleri olmadan yazılacak, eğer yazarsak parantezi otomatik olarak çağırıp çalıştıracak. biz tıklanınca istiyoruz
+
+//? 4. Yol dışarıdan fonksiyon çağırarak
+
+buton1.addEventListener("click",clicked)
+
+function clicked () {
+    sonuc.textContent = "Buton 1 clicked function called from outside"
+}
+
+
 
 
 
