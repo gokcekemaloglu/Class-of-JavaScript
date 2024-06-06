77ec21a00a9bd76c42b9189f914d3c207d320d08
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-06-06 11:00:57 +0300

    "Added Rock-Paper-Scissors game logic to app1.js, including selectors, colors, variables, event listener for user selection, and function to generate PC's random selection."

---
+++ DE09/javascript/classnotes/js18-Rock-Paper-Scissors/app1.js
@@ -0,0 +1,44 @@
+//& Seçim Selectors
+
+let yourChoiceDiv=document.getElementById("your-choice")
+const pcChoiceDiv = document.getElementById("pc-choice")
+
+//& Colors
+const YELLOW = "#FFC538";
+const RED = "#FB778B";
+const GREEN = "#5AB7AC";
+
+//& Variables
+const userSelectImg = document.createElement("img")
+let userSelection; // kullanıcının seçtiği
+let pcRandom; // pc'nin rastgele ürettiği
+let pcArr = [] // pc seçim dizisi
+
+//! Selection
+
+const selectionArticle = document.querySelector(".selection")
+
+selectionArticle.addEventListener("click",(e)=>{
+    // event içindeki target bizim için önemli js kendi oluşturduğu bir durum. hatta daha net olarak id seçiyoruz
+    // console.log(e.target.id); // e dediğimiz event aslında
+    userSelection = e.target.id
+    // console.log(userSelection); //kontrol için yazdık
+        
+    if (userSelection) {
+        userSelectImg.src =`./assets/${userSelection}.png`
+        yourChoiceDiv.appendChild(userSelectImg)
+    }
+    createPCSelection()
+}) 
+
+const createPCSelection = () => {
+    pcArr = ["rock", "paper", "scissor"]
+    pcRandom = pcArr[Math.floor(Math.random() * 3)] //1. yöntem
+    // pcRandom = pcArr[Math.trunc(Math.random() * 3)] //2. yöntem
+    console.log(pcRandom); //kontrol için yazdık
+}
+
+
+
+
+
