46bbc21d6a4f97700a0e56d9aef8fc34d571e116
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-06-06 12:12:16 +0300

    Added score tracking, modal, and result calculation logic to Rock-Paper-Scissors game

--- DE09/javascript/classnotes/js18-Rock-Paper-Scissors/app1.js
+++ DE09/javascript/classnotes/js18-Rock-Paper-Scissors/app1.js
@@ -3,6 +3,10 @@
 let yourChoiceDiv=document.getElementById("your-choice")
 const pcChoiceDiv = document.getElementById("pc-choice")
 
+//& message
+const messagePar = document.querySelector(".message")
+
+
 //& Colors
 const YELLOW = "#FFC538";
 const RED = "#FB778B";
@@ -14,6 +18,18 @@ const pcSelectImg = document.createElement("img")
 let userSelection; // kullanıcının seçtiği
 let pcRandom; // pc'nin rastgele ürettiği
 let pcArr = [] // pc seçim dizisi
+
+//& Score 
+const scoreCardSection = document.querySelector(".score-card")
+const pcScoreSpan = document.getElementById("pc-score")
+const yourScoreSpan = document.getElementById("your-score")
+const domTopscore = document.getElementById("top-score")
+
+//& Modal
+
+const modalCardSection = document.querySelector(".modal-card")
+const finalMessagePar = document.getElementById("final-message")
+const playAgainButton = document.getElementById("play-again")
 
 //! Selection
 
@@ -39,8 +55,54 @@ const createPCSelection = () => {
     // console.log(pcRandom); //kontrol için yazdık
     pcSelectImg.src = `./assets/${pcRandom}.png`
     pcChoiceDiv.appendChild(pcSelectImg)
+    calculateResult()
 }
 
+const calculateResult = () => {
+    console.log(userSelection, pcRandom);
+    if (userSelection === pcRandom) {
+        draw()
+    } else {
+        if (userSelection === "rock") {
+            pcRandom ==="paper" ? youLost() : youWin() //pc paper ise lose, değilse win
+        } else if (userSelection === "scissor"){
+            pcRandom ==="rock" ? youLost() : youWin() 
+        } else if (userSelection === "paper") {
+            pcRandom ==="scissor" ? youLost() : youWin()
+        }
+    }
+    if (pcScoreSpan.textContent ==="10" || yourScoreSpan === "10") {
+        openModal()
+    }
+}
+
+const draw = () => {
+    messagePar.textContent = "It's a draw"
+    scoreCardSection.style.color = YELLOW
+    messagePar.style.backgroundColor = YELLOW
+}
+
+const youLost = ()=> {
+    // console.log("you lost"); // gerek yok
+    messagePar.textContent = "You lost"
+    scoreCardSection.style.color = RED
+    messagePar.style.backgroundColor = RED
+    pcScoreSpan.textContent++
+}
+
+const youWin = ()=> {
+    // console.log("you win"); // gerek yok
+    messagePar.textContent = "You win"
+    scoreCardSection.style.color = GREEN
+    messagePar.style.backgroundColor = GREEN
+    yourScoreSpan.textContent++
+}
+
+const openModal = () => {
+
+    console.log(modalCardSection.classList);
+    
+}
 
 
 
