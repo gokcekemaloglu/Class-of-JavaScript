e4a53a83bd9a95d3ff5f13fe98e074c11d422200
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-06-14 11:27:09 +0300

    Added harcama form handling, harcama list, and formatted gelirler display

--- DE09/javascript/classnotes/js21-Wallet-App/app.js
+++ DE09/javascript/classnotes/js21-Wallet-App/app.js
@@ -11,31 +11,59 @@ const giderinizTd = document.getElementById("gideriniz")
 const kalanTd = document.getElementById("kalan")
 
 //^ Variables
+
 let gelirler = 0
+let harcamaListesi = []
+
+//^ Harcama Formu
+
+const harcamaFormu = document.getElementById("harcama-formu")
+const harcamaAlaniInput = document.getElementById("harcama-alani")
+const tarihInput = document.getElementById("tarih")
+const harcamaInput = document.getElementById("miktar")
 
 
 //? Ekle Formu
 
 ekleFormu.addEventListener("submit", (e) => {
-    e.preventDefault()
+    e.preventDefault() // reload'u önler
     gelirler = gelirler + Number(gelirInput.value)
-    console.log(gelirler);
+    // console.log(gelirler);
     // gelirInput.value = ""
     ekleFormu.reset()
     localStorage.setItem("gelirler", gelirler)
-    gelirinizTd.textContent = gelirler
+    gelirinizTd.textContent = new Intl.NumberFormat().format(gelirler)
 
 })
 
 window.addEventListener("load", () => {
-    gelirler = Number(localStorage.getItem("gelirler"))
-    gelirinizTd.textContent = gelirler
+    gelirler = Number(localStorage.getItem("gelirler")) || 0
+    gelirinizTd.textContent = new Intl.NumberFormat().format(gelirler)
+    tarihInput.valueAsDate = new Date()
+
 })
 
 
 //* DOM
 
+harcamaFormu.addEventListener("submit", (e) => {
+    e.preventDefault() // reload'u önler
+    // console.log(tarihInput.value);
 
+    const yeniHarcama = {
+        id: new Date().getTime(), //! sistem saatini milisaniye olarak verir
+        tarih: new Date(tarihInput.value).toLocaleDateString(),
+        miktar: harcamaInput.value,
+        alan: harcamaAlaniInput.value,
+
+    }
+    // console.log(yeniHarcama);
+    // console.log(yeniHarcama.tarih);
+    harcamaListesi.push(yeniHarcama)
+    console.log(harcamaListesi);
+    harcamaFormu.reset()
+    tarihInput.valueAsDate = new Date()
+})
 
 
 
