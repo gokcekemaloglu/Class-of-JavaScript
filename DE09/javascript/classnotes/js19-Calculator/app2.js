124268c50ad4693832d7a5d13f040bd0d3f8d2b9
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-07-01 12:01:26 +0300

    Refactored calculator app logic, updated event handlers and conditional statements for number and operation buttons, and modified ekran update functions.

--- DE09/javascript/classnotes/js19-Calculator/app2.js
+++ DE09/javascript/classnotes/js19-Calculator/app2.js
@@ -12,78 +12,91 @@ const altEkranDiv = document.querySelector(".current-display");
 //?operator değişkenleri (*******bu şekil global tutmayıp sürekli textcontent te değişiklik yaparsak işlem girildikten sonra, if ile üst ekrana geçer ya sayılar, işlem sonrası bir daha altta sayı göremeyiz ve sürekli uzun uzun yazmamak için, ve işlem e diğer fonksiyonlardan erişemeyiz)
 let ustEkranText = "";
 let altEkranText = "";
- let islem = "";
+let islem = "";
 
 
 
 //?*********** herhangi bir number a basılınca
 
 numberButtons.forEach((number) => {
- 
+    number.onclick = () => {
+        ekranaHazirlik(number.textContent)
+        updateEkran()
+    }
 
 });
 
 //!EKRANA HAZIRLIK
 
 const ekranaHazirlik = (num) => {
-  //todo kullanıcı 0 girerse, sonrasında 0 ve . dışında bir sayı girerse, ekranda sadece girilen yeni sayı (0 iptal olsun) gözüksün
+  //todo: Kullanıcı 0 girerse, sonrasında 0 ve . dışında bir sayı girerse, ekranda sadece girilen yeni sayı (0 iptal olsun) gözüksün
 
+    if (altEkranText === "0" && num !== "0" && num !== ".") {
+        altEkranText = num
 
+        //! bu döngüden çık bu işini globaldeki değişkeni değiştirerek bitirdi ama bişey döndürmeyecek, daha önceki 0 ı da yok sayacak.yoksa 2 ye bas mesela 22 yazıyor, basılan 2 yi yok say, varolan 0 ı 2 yap
 
+        return
+    }
 
+  //todo: Kullanıcı herhangi bir yerde . girmişken, tekrar nokta girmeye kalkarsa giremesin
 
-  //todo kullanıcı herhangi bir yerde . girmişken, tekrar nokta girmeye kalkarsa giremesin
+    if (num === "." && altEkranText.includes(".")) return
 
+  //todo: Kullanıcı 10 haneden sonra girmesin
 
+    if (altEkranText.length > 9) return
 
-  //todo kullanıcı 10 haneden sonra girmesin
+  //todo: Kullanıcı ilk başta 0 girer ardından tekrar 0 girerse, girilmesin, tek 0 döndürsün
 
+    if (num === "0" && altEkranText === "0") return
 
-  //todo kullanıcı ilk başta 0 girer ardından tekrar 0 girerse, girilmesin, tek 0 döndürsün
+  //? Bütün şartları başarı ile geçtiyse basılan numaraları arka arkaya ekle
 
-
-
-
-
-  //?bütün şartları başarı ile geçtiyse basılan numaraları arka arkaya ekle
-
-
+    altEkranText += num
 };
 
 //!BURADA YAPILANLARI EKRANA BASTIR
 
 const updateEkran = () => {
+    altEkranDiv.textContent = altEkranText; 
 
-
- 
-
-  //?işlem girilince
-
-  
-
+  //? işlem girilince
+  // üstekrandaaltta yazan rakam + işlem görünmeli 
+    if (islem) {
+        ustEkranDiv.textContent = `${ustEkranText} ${islem}`
+    } else ustEkranDiv.textContent = ""  
+    
 };
+
 //?**************HERHANGİ BİR İŞLEME TIKLANDIĞINDA
 
 operationButtons.forEach((op) => {
   op.onclick = () => {
-    //?currentDisplay boşken, hiçbir şekilde sayı girişi yapılmamışsa, operatöre basılmasın. boş return yapmaya çalıştığınız işlemi yaptırmaz.
+    //? altEkranDiv boşken, hiçbir şekilde sayı girişi yapılmamışsa, operatöre basılmasın. Boş return, yapmaya çalıştığınız işlemi yaptırmaz.
+    //? return, fonksiyon içerisinde her yerde kullanılabilir. Kod return satırına eriştiğinde fonksiyon durur ve değer fonksiyonun çağırıldığı yere geri gönderilir. Bir fonksiyon içerisinde birden fazla return fonksiyonu da olabilir. return değer döndürmek zorunda değildir. boş return den sonra else if yerine if tercih etmeliyiz
     
+    if (altEkranText === "") return
 
+    //todo: Eşittire basılmadan arka arkaya işleme basılırsa (alt ve üst ekran doluyken işleme basılmaya devam edilirse)
 
-
-    //todo eşittire basılmadan arka arkaya işleme basılırsa (alt ve üst ekran doluyken işleme basılmaya devam edilirse)
+    if (altEkranText && ustEkranText) hesapla()
     
+    islem = op.textContent
+    ustEkranText = altEkranText 
+    altEkranText = ""
 
-
-
+    updateEkran()
   };
 });
+
 //?**************eşittir butonuna tıklandığında
 
 equalButtons.onclick = () => {
+    
 
-
-
+    hesapla()
+    updateEkran()
 };
 
 //! HESAPLA FONKSİYONU
@@ -106,9 +119,13 @@ const hesapla = () => {
       break;
   }
 
+  altEkranText = sonuc
+  ustEkranText = ""
+  islem = ""
 };
 
 //?AC butonuna basıldığında
+
 document.querySelector(".ac").onclick=()=>{
 islem=""
 altEkranText=""
